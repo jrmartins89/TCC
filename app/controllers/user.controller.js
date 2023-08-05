bbe1e9d9f030117f22e4bee8458724715dfd314e
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-05 19:50:22 -0300

    create route to display moderator content.

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -10,3 +10,7 @@ exports.userBoard = (req, res) => {
 exports.adminBoard = (req, res) => {
     res.status(200).send("Admin Content.");
 };
+
+exports.moderatorBoard = (req, res) => {
+    res.status(200).send("Moderator Content.");
+};

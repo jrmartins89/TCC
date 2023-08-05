b540deb0259ecfbbe44cd4ce60baab299db920b2
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-05 19:33:42 -0300

    added function to middleware to check if user is admin.

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -23,3 +23,32 @@ verifyToken = (req, res, next) => {
             next();
         });
 };
+
+isAdmin = (req, res, next) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        Role.find(
+            {
+                _id: { $in: user.roles },
+            },
+            (err, roles) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "admin") {
+                        next();
+                        return;
+                    }
+                }
+                res.status(403).send({ message: "Require Admin Role!" });
+            }
+        );
+    });
+};

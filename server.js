47c2e9891238d04a3b622e312996c0103c1d9452
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-05 18:26:41 -0300

    added first app config.

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const cors = require("cors");
 const cookieSession = require("cookie-session");
 const app = express();
 
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+//app configs.
+app.use(cors(corsOptions)); // user cors middleware
+app.use(express.json()); // parse requests of content-type - application/json
+app.use(express.urlencoded({ extended: true })); // parse requests of content-type - application/x-www-form-urlencoded

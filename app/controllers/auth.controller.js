b96f8bc729c7309ad063be4ac8f5d2aa589ef583
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-05 19:44:34 -0300

    added signup function to controller file.

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -5,3 +5,59 @@ const Role = db.role;
 
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
+
+exports.signup = (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8),
+    });
+
+    user.save((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
+        }
+
+        if (req.body.roles) {
+            Role.find(
+                {
+                    name: { $in: req.body.roles },
+                },
+                (err, roles) => {
+                    if (err) {
+                        res.status(500).send({ message: err });
+                        return;
+                    }
+
+                    user.roles = roles.map((role) => role._id);
+                    user.save((err) => {
+                        if (err) {
+                            res.status(500).send({ message: err });
+                            return;
+                        }
+
+                        res.send({ message: "User was registered successfully!" });
+                    });
+                }
+            );
+        } else {
+            Role.findOne({ name: "user" }, (err, role) => {
+                if (err) {
+                    res.status(500).send({ message: err });
+                    return;
+                }
+
+                user.roles = [role._id];
+                user.save((err) => {
+                    if (err) {
+                        res.status(500).send({ message: err });
+                        return;
+                    }
+
+                    res.send({ message: "User was registered successfully!" });
+                });
+            });
+        }
+    });
+};

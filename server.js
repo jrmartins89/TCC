465a6e39fbc28a3991c5df9779acd3c53ecd4b4b
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-05 18:31:14 -0300

    update cookie session configs.

--- server.js
+++ server.js
@@ -11,3 +11,10 @@ var corsOptions = {
 app.use(cors(corsOptions)); // user cors middleware
 app.use(express.json()); // parse requests of content-type - application/json
 app.use(express.urlencoded({ extended: true })); // parse requests of content-type - application/x-www-form-urlencoded
+app.use(                                                // helps to store the session data on the client within a cookie without requiring any database/resources on the server side
+    cookieSession({
+        name: "vagouAqui-session",
+        keys: ["COOKIE_SECRET"], // should use as secret environment variable
+        httpOnly: true
+    })
+);

ef4f0f3fca32d2df8629cf630909c8927fdaf65e
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-06 00:33:49 -0300

    added logout function for authentication actions.

--- app/client/src/actions/auth.js
+++ app/client/src/actions/auth.js
@@ -75,3 +75,12 @@ export const login = (username, password) => (dispatch) => {
         }
     );
 };
+
+
+export const logout = () => (dispatch) => {
+    AuthService.logout();
+
+    dispatch({
+        type: LOGOUT,
+    });
+};

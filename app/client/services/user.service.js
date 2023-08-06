1ab3e20fcf07dfebb8517a5a97589ecda33cae23
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-06 00:16:54 -0300

    added function to access user data using axios.

--- app/client/services/user.service.js
+++ app/client/services/user.service.js
@@ -6,3 +6,7 @@ const API_URL = "http://localhost:8080/api/test/";
 const getPublicContent = () => {
     return axios.get(API_URL + "all");
 };
+
+const getUserBoard = () => {
+    return axios.get(API_URL + "user", { headers: authHeader() });
+};

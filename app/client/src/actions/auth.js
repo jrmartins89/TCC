f41a21e631a1d3f5c4b0a1df4fd6c6862a6691e0
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-06 00:33:26 -0300

    added login function for authentication actions.

--- app/client/src/actions/auth.js
+++ app/client/src/actions/auth.js
@@ -43,3 +43,35 @@ export const register = (username, email, password) => (dispatch) => {
         }
     );
 };
+
+export const login = (username, password) => (dispatch) => {
+    return AuthService.login(username, password).then(
+        (data) => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: { user: data },
+            });
+
+            return Promise.resolve();
+        },
+        (error) => {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+
+            dispatch({
+                type: LOGIN_FAIL,
+            });
+
+            dispatch({
+                type: SET_MESSAGE,
+                payload: message,
+            });
+
+            return Promise.reject();
+        }
+    );
+};

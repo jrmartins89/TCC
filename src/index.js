001a1b2b9a5adde61705ca78aedcf50bbbe83534
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-06 20:28:58 -0300

    updated path for source code of frontend.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./app";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";

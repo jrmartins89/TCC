6369eb884432813f5eef0356f6c68328a5fdae19
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-06 01:46:46 -0300

    change index file to wrap application with a <Provider> component. It makes the Redux store available to any nested components.

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );

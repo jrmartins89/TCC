3c48448458c00d21e9d0ea5b3607bfff000055c5
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-05 23:44:40 -0300

    added firs render to index.

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -4,3 +4,11 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);

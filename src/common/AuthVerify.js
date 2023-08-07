87d5dd5a36a55a550e1b1e04e9d54e56121a60a2
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-07 19:18:40 -0300

    fixed authVerify function

--- src/common/AuthVerify.js
+++ src/common/AuthVerify.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
 
 const parseJwt = (token) => {
     try {
@@ -25,4 +25,17 @@ const AuthVerify = (props) => {
     return <div></div>;
 };
 
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
+}
 export default withRouter(AuthVerify);
